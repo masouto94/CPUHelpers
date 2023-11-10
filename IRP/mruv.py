98d08ffb468acdca4cf5df4816ed5f8546872c86
Matias Souto <matias.souto@avature.net>
2023-11-10 16:50:38 -0300

    Mruv file

---
+++ IRP/mruv.py
@@ -0,0 +1,16 @@
+gabriel = 3
+florencia = 2
+mts_gabriel = 10
+mts_florencia= 0
+
+for i in range(100):
+    print(f"Gabriel va a {gabriel} m/s lleva {mts_gabriel} metros recorridos al segundo {i}")
+    print(f"Florencia va a {round(float(florencia),3)} m/s lleva {round(float(mts_florencia),3)} metros recorridos al segundo {i}")
+    if mts_florencia >= mts_gabriel:
+        print("####")
+        print(f"Florencia lo alcanzó para el segundo {i}")
+        print("####")
+        break
+    mts_gabriel += gabriel
+    mts_florencia += florencia
+    florencia += 0.4
