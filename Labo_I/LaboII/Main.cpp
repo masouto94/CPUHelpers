76a122a3888f362b6a3c3d71356ab8e9a6a7b884
Matias Souto <masouto94@gmail.com>
2024-04-25 11:40:48 -0300

    last

--- Labo_I/LaboII/Main.cpp
+++ Labo_I/LaboII/Main.cpp
@@ -1,7 +1,8 @@
 #include "../Labo-1-C/Parciales/notas.h"
 #include "../Labo-1-C/Parciales/piedraPapelTijera.h"
+#include "../Labo-1-C/Parciales/letterSwapper.h"
 int main(){
-    PiedraPapelTijera();
+    LetterSwapper();
 
 
 
