47179f3c86ed576db2ac7a11ed3a61edc73dc47b
Matias Souto <masouto94@gmail.com>
2024-06-12 22:45:51 -0300

    Sumando las musicas nuevas

--- Labo_I/ArduinoProjects/secretCodeProject/music.h
+++ Labo_I/ArduinoProjects/secretCodeProject/music.h
@@ -1,51 +1,95 @@
 // NOTES
 #define BUZZER 2
 #define NOTE_SILENCE 0
-#define NOTE_A6 1760
-#define NOTE_F6 1397
-#define NOTE_C7 2093
-#define NOTE_DS5 622
-#define NOTE_B4 494
-#define NOTE_CS5 554
-void funeralMarch()
-{
-  int funeralMelody[] = {
-      NOTE_A6, NOTE_A6, NOTE_A6, NOTE_F6, NOTE_A6, NOTE_A6, NOTE_F6, NOTE_A6,
-      NOTE_A6, NOTE_F6, NOTE_C7, NOTE_A6, NOTE_F6, NOTE_C7, NOTE_A6};
-  int funeralNoteDurations[] = {
-      4, 8, 4, 4, 8, 4, 4, 8,
-      4, 4, 4, 4, 8, 4, 4};
-  int melodyLength = sizeof(funeralMelody) / sizeof(int);
-  for (int thisNote = 0; thisNote < melodyLength; thisNote++)
-  {
-    int noteDuration = 1000 / funeralNoteDurations[thisNote];
-    tone(BUZZER, funeralMelody[thisNote], noteDuration);
-    int pauseBetweenNotes = noteDuration * 1.30;
-    delay(pauseBetweenNotes);
+#define NOTE_F5  698
+#define NOTE_G5  784
+#define NOTE_A5  880
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_G6  1568
+
+void introMusic() {
+  int melody[] = {
+    NOTE_G5, NOTE_A5, NOTE_D6, NOTE_G6
+  };
+
+  int noteDurations[] = {
+    8, 8, 8, 4
+  };
+
+  int melodyLength = sizeof(melody) / sizeof(int);
+  for (int thisNote = 0; thisNote < melodyLength; thisNote++) {
+    int noteDuration = 1500 / noteDurations[thisNote];
+    
+    if (melody[thisNote] != NOTE_SILENCE) {
+        tone(BUZZER, melody[thisNote], noteDuration);
+    }
+    delay(noteDuration);
+
     noTone(BUZZER);
+
+    int pauseBetweenNotes = noteDuration * 0.30;
+    delay(pauseBetweenNotes);
   }
 }
-void finalFantasy()
-{
+
+void funeralMarch() {
+  int funeralMelody[] = {
+    NOTE_A5, NOTE_A5, NOTE_A5, NOTE_F5, 
+    NOTE_C6, NOTE_A5, NOTE_F5, NOTE_C6, NOTE_A5,
+  };
+
+  int funeralNoteDurations[] = {
+    4, 4, 4, 4, 
+    12, 4, 4, 12, 4
+  };
+
+  int melodyLength = sizeof(funeralMelody) / sizeof(int);
+  for (int thisNote = 0; thisNote < melodyLength; thisNote++) {
+    int noteDuration = 2000 / funeralNoteDurations[thisNote];
+    
+    if (funeralMelody[thisNote] != NOTE_SILENCE) {
+        tone(BUZZER, funeralMelody[thisNote], noteDuration);
+    }
+    delay(noteDuration);
+
+    noTone(BUZZER);
+
+    int pauseBetweenNotes = noteDuration * 0.30;
+    delay(pauseBetweenNotes);
+  }
+}
+
+void finalFantasy() {
   int melody[] = {
-      NOTE_DS5, NOTE_DS5, NOTE_DS5, NOTE_DS5, NOTE_B4,
-      NOTE_CS5, NOTE_DS5, NOTE_SILENCE, NOTE_CS5, NOTE_DS5};
+    NOTE_DS6, NOTE_DS6, NOTE_DS6, NOTE_DS6, NOTE_B5,
+    NOTE_CS6, NOTE_DS6, NOTE_SILENCE, NOTE_CS6, NOTE_DS6
+  };
+
   // Duraciones de las notas: 4 = negra, 8 = corchea, etc.
   int noteDurations[] = {
-      16, 16, 4, 4, 8,
-      4, 16, 8, 8, 1};
-  for (int thisNote = 0; thisNote < 10; thisNote++)
-  {
+    16, 16, 16, 4, 4, 
+    4, 16, 8, 8, 1
+  };
+  
+  int melodyLength = sizeof(melody) / sizeof(int);
+  for (int thisNote = 0; thisNote < melodyLength; thisNote++) {
+
     int noteDuration = 1500 / noteDurations[thisNote];
     // Si la nota no es un silencio, toca la nota
-    if (melody[thisNote] != NOTE_SILENCE)
-    {
+    if (melody[thisNote] != NOTE_SILENCE) {
       tone(BUZZER, melody[thisNote], noteDuration);
     }
+    
     // Espera la duración de la nota
     delay(noteDuration);
+
     // Detiene el tono después de la duración de la nota
     noTone(BUZZER);
+
     // Pausa entre notas para mejorar la separación
     int pauseBetweenNotes = noteDuration * 0.30;
     delay(pauseBetweenNotes);
